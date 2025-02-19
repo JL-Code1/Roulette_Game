b0491933b8c1318069bd44e1117e0eb086118c59
Jordan Lorts <jordanjlorts@gmail.com>
2025-02-19 17:31:41 -0600

    frontend & backend npm init -y

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const setupGraphQL = require("./config/graphql");
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Set up GraphQL
+setupGraphQL(app);
+
+const PORT = process.env.PORT || 5001; // 5000 not working
+
+app.listen(PORT, () => {
+    console.log(`✅ Server running on port ${PORT}`);
+});
